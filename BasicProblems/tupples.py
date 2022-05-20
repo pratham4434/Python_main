96d48723c71b8ebe794399a4755473c97254c5ce
Pratham <prathamsharmafaangm@gmail.com>
2022-05-20 14:55:50 +0530

    tupples.py  added

--- BasicProblems/tupples.py
+++ BasicProblems/tupples.py
@@ -2,22 +2,22 @@ courses = ["History", "Math", "Physics", "CompSci"]
 courses_2 = ["Art", "Education"]
 
 print(courses)
-print(len(courses))
-print(courses[0])  # first item
-print(courses[-1]) # starts frm the end of the list
-print(courses[0 : 2]) # all the way upto bt not including the last one
+print(len(courses))     # length of the array
+print(courses[0])       # first item
+print(courses[-1])      # starts frm the end of the list
+print(courses[0 : 2])   # all the way upto bt not including the last one
 
 
 courses.append('Art')
-print(courses)  
+print(courses)          # inserts an item at the last
 
-courses.insert(0,'English')
+courses.insert(0,'English') # inserts at the exact place
 print(courses)
 
-courses.insert(0, courses_2)
+courses.insert(0, courses_2) # inserts the whole array
 print(courses)
 
-courses.extend(courses_2)
+courses.extend(courses_2) # inserts the contents of the whole array
 print(courses)
 
 courses.remove()   # removes the exact item
